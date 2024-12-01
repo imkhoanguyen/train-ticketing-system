c6a0ab8ae11a1a582c072da7149c9ad3af7962af
vietdai134 <vietnguyentran134@gmail.com>
2024-12-02 00:09:09 +0700

    fix admin UI

--- backend/train/src/main/java/com/example/train/repository/DiscountRepository.java
+++ backend/train/src/main/java/com/example/train/repository/DiscountRepository.java
@@ -1,7 +1,6 @@
 package com.example.train.repository;
 
 import com.example.train.entity.Discount;
-import com.example.train.entity.Promotion;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
--- backend/train/src/main/java/com/example/train/repository/UserRepository.java
+++ backend/train/src/main/java/com/example/train/repository/UserRepository.java
@@ -1,10 +1,7 @@
 package com.example.train.repository;
 
-import com.example.train.entity.Promotion;
 import com.example.train.entity.User;
 import com.example.train.repository.custom.CustomUserRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
