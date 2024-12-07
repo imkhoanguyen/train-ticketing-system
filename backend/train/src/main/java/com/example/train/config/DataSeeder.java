f04458c9b3148c15f0e9e271b06ad173888f3167
khoa <khoanguyen.coder@gmail.com>
2024-12-07 11:58:48 +0700

    refactor: add data seeder (promotion and discount)

--- backend/train/src/main/java/com/example/train/config/DataSeeder.java
+++ backend/train/src/main/java/com/example/train/config/DataSeeder.java
@@ -1,12 +1,19 @@
 package com.example.train.config;
 
+import com.example.train.entity.Discount;
+import com.example.train.entity.Promotion;
 import com.example.train.entity.User;
+import com.example.train.repository.DiscountRepository;
+import com.example.train.repository.PromotionRepository;
 import com.example.train.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
 
 @RequiredArgsConstructor
 @Configuration
@@ -15,7 +22,7 @@ public class DataSeeder {
     private final PasswordEncoder passwordEncoder;
 
     @Bean
-    CommandLineRunner seedData(UserRepository userRepository) {
+    CommandLineRunner seedData(UserRepository userRepository, DiscountRepository discountRepository, PromotionRepository promotionRepository) {
         return args -> {
             if (userRepository.count() == 0) {
                 User user = new User();
@@ -37,6 +44,60 @@ public class DataSeeder {
                 user1.setRole("Customer");
                 user1.setCmnd("147258369");
                 userRepository.save(user1);
+            }
+
+            if(discountRepository.count() == 0) {
+                Discount discount = new Discount();
+                discount.setPrice(new BigDecimal("10000"));
+                discount.setObject("Người cao tuổi");
+                discount.setDescription("Khách hàng lớn tuổi");
+                discountRepository.save(discount);
+
+                Discount discount1 = new Discount();
+                discount.setPrice(new BigDecimal("5000"));
+                discount.setObject("Trẻ em");
+                discount.setDescription("Giảm giá cho trẻ em");
+                discountRepository.save(discount1);
+
+                Discount discount2= new Discount();
+                discount.setPrice(new BigDecimal("0"));
+                discount.setObject("Người lớn");
+                discount.setDescription("Giảm giá cho người lớn");
+                discountRepository.save(discount2);
+            }
+
+            if (promotionRepository.count() == 0) {
+                // Tạo đối tượng Promotion đầu tiên
+                Promotion promotion1 = new Promotion();
+                promotion1.setPrice(new BigDecimal("50000"));
+                promotion1.setName("Summer Sale 2024");
+                promotion1.setDescription("Giảm giá mùa hè 2024");
+                promotion1.setCode("SUMMER2024");
+                promotion1.setCount(100);
+                promotion1.setStartDate(ZonedDateTime.now());
+                promotion1.setEndDate(ZonedDateTime.now().plusMonths(1)); // Ngày kết thúc là một tháng sau
+                promotionRepository.save(promotion1);
+
+                Promotion promotion2 = new Promotion();
+                promotion2.setPrice(new BigDecimal("10000"));
+                promotion2.setName("Black Friday Deal");
+                promotion2.setDescription("Giảm giá nhân dịp Black Friday");
+                promotion2.setCode("BLACKFRIDAY2024");
+                promotion2.setCount(50);
+                promotion2.setStartDate(ZonedDateTime.now());
+                promotion2.setEndDate(ZonedDateTime.now().plusMonths(2));
+                promotionRepository.save(promotion2);
+
+                // Tạo đối tượng Promotion thứ ba
+                Promotion promotion3 = new Promotion();
+                promotion3.setPrice(new BigDecimal("30000"));
+                promotion3.setName("Christmas Sale");
+                promotion3.setDescription("Khuyến mãi Giáng Sinh");
+                promotion3.setCode("CHRISTMAS2024");
+                promotion3.setCount(200);
+                promotion3.setStartDate(ZonedDateTime.now());
+                promotion3.setEndDate(ZonedDateTime.now().plusMonths(3));
+                promotionRepository.save(promotion3);
             }
         };
     }
